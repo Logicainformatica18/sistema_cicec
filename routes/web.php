3203b974eeeba596b18902416c4314483f082737
Anthony <logicainformatica18@gmail.com>
2024-06-17 18:44:15 -0500

    plantilla terminada

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+Route::get('/perfil', function () {
+    return view('template');
 });
 
 
